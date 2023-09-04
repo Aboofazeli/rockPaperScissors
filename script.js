4b74749b05f7cb2932a9ce5cd4df6e5b85d6be5b
Ali Reza Aboofazeli <20898342+Aboofazeli@users.noreply.github.com>
2023-09-04 10:40:07 -0400

    ui

--- script.js
+++ script.js
@@ -21,33 +21,6 @@ function playRound(playerSelection, computerSelection) {
     return winner;
 }
 
-function game() {
-    let i = 0;
-    let user_score = 0;
-    let comp_score = 0;
-    while (i < 5) {
-        player = getPlayerSelection();
-        computer = getComputerSelection();
-        winner = playRound(player, computer);
-        if (winner == "User") {
-            user_score ++;
-        }
-        else if (winner == "Computer") {
-            comp_score ++;
-        }
-        console.log(`Your Score: ${user_score}, Computer Score: ${comp_score} `)
-        i++;
-    }
-    if (user_score > comp_score) {
-        console.log(`User: ${user_score}, Computer: ${comp_score}, You Win!`)
-    }
-    else if (comp_score > user_score) {
-        console.log(`User: ${user_score}, Computer: ${comp_score}, You Lose!`)
-    }
-    else {
-        console.log(`User: ${user_score}, Computer: ${comp_score}, Tie!`)
-    }
-}
 function getComputerSelection() {
     const options = ["Rock", "Paper", "Scissor"];
     randomSelect = Math.floor(Math.random()*3);
@@ -68,4 +41,64 @@ function getPlayerSelection() {
 
 
 const computerSelection = getComputerSelection();
-game();
+
+let user_score = 0;
+let comp_score = 0;
+let final_winner;
+const userElement = document.createElement('p');
+const compElement = document.createElement('p');
+const winnerElement = document.createElement('p');
+const compSelectionElement = document.createElement('p');
+const buttons = document.querySelectorAll('.button')
+console.log(buttons);
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        final_winner = null;
+        winnerElement.textContent = "";
+        let playerSelection = button.id;
+        let computerSelection = getComputerSelection();
+        const container = document.querySelector('.computer');
+        compSelectionElement.textContent = `Computer: ${computerSelection}`;
+        compSelectionElement.style.fontSize = '30px';
+        compSelectionElement.style.padding = '5px';
+        compSelectionElement.style.textAlign = 'center';
+        container.appendChild(compSelectionElement);
+
+        let winner = playRound(playerSelection, computerSelection)
+        if (winner === "User") user_score++;
+        else if (winner === "Computer") comp_score++;
+
+        userElement.textContent = `Your score: ${user_score}`;
+        userElement.style.padding = '5px';
+        compElement.textContent = `Computer score: ${comp_score}`;
+        compElement.style.padding = '5px';
+        container.appendChild(userElement);
+        container.appendChild(compElement);
+
+        if (user_score === 5 && comp_score !== 5) final_winner = "You Won!";
+        else if (comp_score === 5 && user_score !== 5) final_winner = "You Lost!";
+
+        if (user_score === 5 || comp_score === 5) {
+            if (user_score === 5) final_winner = "You Won!";
+            else final_winner = "You Lost!";
+            resetGame(); // Reset the game when someone wins
+            winnerElement.textContent = final_winner;
+            winnerElement.style.fontSize = '50px';
+            winnerElement.style.color = 'black';
+            winnerElement.style.fontFamily = 'Arial, Helvetica, sans-serif';
+            winnerElement.style.textAlign = 'center';
+            winnerElement.style.padding = '5px';
+            container.appendChild(winnerElement);
+        }
+
+    })
+});
+
+function resetGame() {
+    user_score = 0;
+    comp_score = 0;
+    userElement.textContent = `Your score: ${user_score}`;
+    compElement.textContent = `Computer score: ${comp_score}`;
+    compSelectionElement.textContent = "";
+}
+
